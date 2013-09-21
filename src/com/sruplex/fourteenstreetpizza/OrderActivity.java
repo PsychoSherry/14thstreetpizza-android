290755aae72fe92d6f4b0265fc43e92cc5ff958c
Sheharyar Naseer <sheharyar92@live.com>
2013-09-21 18:19:50 +0500

    Arrays

--- src/com/sruplex/fourteenstreetpizza/OrderActivity.java
+++ src/com/sruplex/fourteenstreetpizza/OrderActivity.java
@@ -2,58 +2,57 @@ package com.sruplex.fourteenstreetpizza;
 
 import android.app.ActionBar;
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
-import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.LinearLayout.LayoutParams;
 import android.widget.ListView;
-
-import com.commonsware.cwac.merge.MergeAdapter;
 
 
 public class OrderActivity extends Activity {
-	ListView list = null;
+	ListView orderlist = null;
+	int baseheight;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-//		setContentView(R.layout.activity_order);
-		setContentView(R.layout.empty_list);
-
+		setContentView(R.layout.activity_order);
+		
 		String sss[] = new String[3];
 		sss[0] = "Yo Bitch";
 		sss[1] = "sdfds";
 		sss[2] = "sdfsd";
 		
-		LayoutInflater inflater = (LayoutInflater) getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-		View Header = inflater.inflate(R.layout.activity_order, null);
-		
-		MergeAdapter myMergeAdapter = new MergeAdapter();
-		myMergeAdapter.addView(Header);
-
-		myMergeAdapter.addAdapter(new RowAdapter(getApplicationContext(), sss));
-		myMergeAdapter.addAdapter(new RowAdapter(getApplicationContext(), sss));
-		myMergeAdapter.addAdapter(new RowAdapter(getApplicationContext(), sss));
-		
-		list = (ListView) findViewById (R.id.list);
-		list.setAdapter(myMergeAdapter);		
-		
-		
 		// Initialize Objects
-//		orderlist = (ListView) findViewById (R.id.orderlist);
-//		orderlist.setAdapter(new RowAdapter(getApplicationContext(), sss));
+		orderlist = (ListView) findViewById (R.id.orderlist);
+//		baseheight = orderlist.getLayoutParams().height;
+		baseheight = 100;
+
+		orderlist.setLayoutParams(new LayoutParams(orderlist.getLayoutParams().width, (baseheight * sss.length) + 30));
+		orderlist.setAdapter(new RowAdapter(getApplicationContext(), sss));
 		
 		// Modify the ActionBar
         int myapi = android.os.Build.VERSION.SDK_INT;
         if (myapi >= 11) { 
             ActionBar actionBar = getActionBar();
                 actionBar.setDisplayHomeAsUpEnabled(true);
-                actionBar.setDisplayShowHomeEnabled(false); 
+                actionBar.setDisplayShowHomeEnabled(false);
         }        
         
+	}
+
+	public void OrderAdd_Pizza(View view){
+		
+	}
+	
+	public void OrderAdd_Deal(View view){
+		
+	}
+	
+	public void OrderAdd_Menu(View view){
+		
 	}
 	
     @Override
---
+++ src/com/sruplex/fourteenstreetpizza/OrderValues.java
@@ -0,0 +1,14 @@
+package com.sruplex.fourteenstreetpizza;
+
+public class OrderValues {
+
+	public static String[] Title;
+	public static String[] Description;
+	
+	public static Integer[] Quantity;
+	public static Integer[] Price;
+	public static Integer[] TotalCost;
+	
+	public static int[] Image;
+
+}
